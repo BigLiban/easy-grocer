56eb98e271706b2c06aedce8a4323fcb89b654a2
Mohamed Bekdach <75533061+BigLiban@users.noreply.github.com>
2023-05-02 18:57:57 -0400

    scarping changes

--- server/scrapeWalmart.js
+++ server/scrapeWalmart.js
@@ -1,5 +1,17 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-    
+    //Non headless mode, for headless mode, said to true
+    const browser = await puppeteer.launch({headless: false});
+
+    const page = await browser.newPage();
+    await page.goto("https://www.walmart.ca/en");
+
+    await page.click('#typeahead-search-input');
+
+    await wait(5000);
+
+    await browser.close();
+
+
 })();
